124999cce61170b2ebfc0b5a21b1db4fe04398cf
AminForou <73676109+AminForou@users.noreply.github.com>
2024-10-05 16:58:36 -0400

    meta data and canonical fixed

--- app/tools/bulk-url-opener/page.tsx
+++ app/tools/bulk-url-opener/page.tsx
@@ -1,6 +1,20 @@
 import BulkUrlOpener from './BulkUrlOpener';
 import BulkUrlFaq from './components/BulkUrlFaq';
+import { Metadata } from 'next';
 
+const domain = process.env.NEXT_PUBLIC_CANONICAL_URL;
+    
+export const metadata: Metadata = {
+    title: 'Bulk URL Opener - SEO Platform',
+    description:
+      "Have questions or suggestions? Reach out to the SEO Tools Hub team. We're here to help you optimize your SEO workflow and improve your website's performance.",
+      robots: {
+          index: false, // Equivalent to "noindex"
+        },
+        alternates: {
+          canonical: `${domain}/tools/bulk-url-opener`,
+        },
+  };
 export default function BulkUrlOpenerPage() {
   return (
     <div className="container mx-auto px-4 py-8">
--- app/tools/site-structure-analyzer/page.tsx
+++ app/tools/site-structure-analyzer/page.tsx
@@ -1,5 +1,20 @@
 import URLAnalysisDashboard from './URLAnalysisDashboard';
 import SiteStructureFaq from './components/SiteStructureFaq';
+import { Metadata } from 'next';
+
+const domain = process.env.NEXT_PUBLIC_CANONICAL_URL;
+    
+export const metadata: Metadata = {
+    title: 'Site Structure Analyzer - SEO Platform',
+    description:
+      "Have questions or suggestions? Reach out to the SEO Tools Hub team. We're here to help you optimize your SEO workflow and improve your website's performance.",
+      robots: {
+          index: false, // Equivalent to "noindex"
+        },
+        alternates: {
+          canonical: `${domain}/tools/site-structure-analyzer`,
+        },
+  };
 
 export default function BulkUrlOpenerPage() {
   return (
